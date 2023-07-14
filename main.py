663e59ebf65d4168e492981bc3b5e0bff1ff481c
Nick NRGameAce <nregas14@icloud.com>
2023-07-14 15:38:32 -0700

    Started working on map

--- main.py
+++ main.py
@@ -1,1 +1,131 @@
+import pygame, os, time, random
+map_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 
+]
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# class Pacman():
+#         def __init__(self, radius = 15, position = [random.randint(0, width - pacman_radius),
+#                    random.randint(0, height - pacman_radius)], direction =[0,0], speed = 5 ):
+#             self.pacman_radius = radius
+#             self.pacman_position = position
+#             self.pacman_direction = direction
+#             self.pacman_speed = speed
+
+
+class Game():
+        def __init__(self):
+            pygame.init()
+            self.GAME_W,self.GAME_H = 1000, 800
+            self.SCREEN_WIDTH,self.SCREEN_HEIGHT = 1200, 800
+            self.game_canvas = pygame.Surface((self.GAME_W,self.GAME_H))
+            self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,self.SCREEN_HEIGHT))
+            self.running, self.playing = True, True
+            self.dt, self.prev_time = 0, 0
+            self.load_assets()
+            
+        def draw_map(self):
+            for row in range(len(map_data)):
+                for col in range(len(map_data[row])):
+                    tile = map_data[row][col]
+                    x = col * 32  # Adjust the tile size as needed
+                    y = row * 32
+                    if tile == 1:
+                        pygame.draw.rect(self.game_canvas, BLUE, (x, y, 32, 32))
+                    elif tile == 0:
+                        pygame.draw.rect(self.game_canvas, BLACK, (x, y, 32, 32))
+
+        def game_loop(self):
+            while self.playing:
+                self.get_dt()
+                self.get_events()
+                self.update()
+                self.render()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+                        self.playing = False
+                    
+                
+                
+        def get_events(self):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.playing = False
+                    self.running = False
+
+
+        
+        def update(self):
+            self.draw_map()
+            
+
+        def render(self):
+            
+            self.screen.blit(pygame.transform.scale(self.game_canvas,(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0,0))
+            pygame.display.flip()
+
+
+        def get_dt(self):
+            now = time.time()
+            self.dt = now - self.prev_time
+            self.prev_time = now
+
+        def draw_text(self, surface, text, color, x, y):
+            text_surface = self.font.render(text, True, color)
+            text_rect = text_surface.get_rect()
+            text_rect.center = (x, y)
+            surface.blit(text_surface, text_rect)
+
+        def load_assets(self):
+            
+            self.assets_dir = os.path.join("assets")
+            
+            
+
+        def reset_keys(self):
+            for action in self.actions:
+                self.actions[action] = False
+
+g = Game()
+
+while g.running:
+    g.game_loop()
+                
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+
+#     # Clear the screen
+#     Game.screen.fill(BLACK)
+
+    
+#     # Update the display
+#     pygame.display.flip()
+
+#     # Limit the frame rate
+#     Game.clock.tick(60)
+
+# # Quit the game
+# pygame.quit()
+        
